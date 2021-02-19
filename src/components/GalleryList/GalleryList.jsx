eb7045af6fbc1030e85e1083ac6437e0e9b11998
William Krug <william.p.krug@gmail.com>
2021-02-18 22:05:00 -0600

    gallery items got inline-block display

--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,96 +1,134 @@
+import './GalleryList.css';
+
 function GalleryList() {
   return (
     <div>
       <h2>Through the Years</h2>
       <div className="gallery-display">
         <div className="gallery-item">
-          <img
-            src="../../public/images/emma.jpg"
-            alt="Photo of two knights fighting with hand-and-a-half broadswords."
-            width="100"
-            height="100"
-          />
-          <button onClick={() => console.log('Like Button Clicked')}>
-            Love It!
-          </button>
-          <p>
-            <strong>??</strong> people love this!
-          </p>
+          <div>
+            <img
+              src="../../public/images/emma.jpg"
+              alt="Photo of two knights fighting with hand-and-a-half broadswords."
+              width="100"
+              height="100"
+            />
+          </div>
+          <div>
+            <button onClick={() => console.log('Like Button Clicked')}>
+              Love It!
+            </button>
+          </div>
+          <div>
+            <p>
+              <strong>??</strong> people love this!
+            </p>
+          </div>
         </div>
 
         <div className="gallery-item">
-          <img
-            src="../../public/images/skydiving.jpg"
-            alt="Photo of tandem skydivers at 7,000 feet."
-            width="100"
-            height="100"
-          />
-          <button onClick={() => console.log('Like Button Clicked')}>
-            Love It!
-          </button>
-          <p>
-            <strong>??</strong> people love this!
-          </p>
+          <div>
+            <img
+              src="../../public/images/skydiving.jpg"
+              alt="Photo of tandem skydivers at 7,000 feet."
+              width="100"
+              height="100"
+            />
+          </div>
+          <div>
+            <button onClick={() => console.log('Like Button Clicked')}>
+              Love It!
+            </button>
+          </div>
+          <div>
+            <p>
+              <strong>??</strong> people love this!
+            </p>
+          </div>
         </div>
 
         <div className="gallery-item">
-          <img
-            src="../../public/images/digital.jpg"
-            alt="Photo of a color burst digitally rendered."
-            width="100"
-            height="100"
-          />
-          <button onClick={() => console.log('Like Button Clicked')}>
-            Love It!
-          </button>
-          <p>
-            <strong>??</strong> people love this!
-          </p>
+          <div>
+            <img
+              src="../../public/images/digital.jpg"
+              alt="Photo of a color burst digitally rendered."
+              width="100"
+              height="100"
+            />
+          </div>
+          <div>
+            <button onClick={() => console.log('Like Button Clicked')}>
+              Love It!
+            </button>
+          </div>
+          <div>
+            <p>
+              <strong>??</strong> people love this!
+            </p>
+          </div>
         </div>
 
         <div className="gallery-item">
-          <img
-            src="../../public/images/amber.jpg"
-            alt="Photo of a stunning wife on our wedding day."
-            width="100"
-            height="100"
-          />
-          <button onClick={() => console.log('Like Button Clicked')}>
-            Love It!
-          </button>
-          <p>
-            <strong>??</strong> people love this!
-          </p>
+          <div>
+            <img
+              src="../../public/images/amber.jpg"
+              alt="Photo of a stunning wife on our wedding day."
+              width="100"
+              height="100"
+            />
+          </div>
+          <div>
+            <button onClick={() => console.log('Like Button Clicked')}>
+              Love It!
+            </button>
+          </div>
+          <div>
+            <p>
+              <strong>??</strong> people love this!
+            </p>
+          </div>
         </div>
 
         <div className="gallery-item">
-          <img
-            src="../../public/images/munchkins.jpg"
-            alt="Photo of a new big sister loving her little brother."
-            width="100"
-            height="100"
-          />
-          <button onClick={() => console.log('Like Button Clicked')}>
-            Love It!
-          </button>
-          <p>
-            <strong>??</strong> people love this!
-          </p>
+          <div>
+            <img
+              src="../../public/images/munchkins.jpg"
+              alt="Photo of a new big sister loving her little brother."
+              width="100"
+              height="100"
+            />
+          </div>
+          <div>
+            <button onClick={() => console.log('Like Button Clicked')}>
+              Love It!
+            </button>
+          </div>
+          <div>
+            <p>
+              <strong>??</strong> people love this!
+            </p>
+          </div>
         </div>
 
         <div className="gallery-item">
-          <img
-            src="../../public/images/emma.jpg"
-            alt="Photo of two knights fighting with hand-and-a-half broadswords."
-            width="100"
-            height="100"
-          />
-          <button onClick={() => console.log('Like Button Clicked')}>
-            Love It!
-          </button>
-          <p>
-            <strong>??</strong> people love this!
-          </p>
+          <div>
+            <img
+              src="../../public/images/emma.jpg"
+              alt="Photo of two knights fighting with hand-and-a-half broadswords."
+              width="100"
+              height="100"
+            />
+          </div>
+          <div>
+            <button onClick={() => console.log('Like Button Clicked')}>
+              Love It!
+            </button>
+          </div>
+          <div>
+            <p>
+              <strong>??</strong> people love this!
+            </p>
+          </div>
         </div>
       </div>
     </div>
