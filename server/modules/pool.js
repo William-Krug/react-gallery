e42bd4339700c9a7e0922aa532c755fd5c0acf0c
William Krug <william.p.krug@gmail.com>
2021-02-20 13:53:35 -0600

    updated gallery.router.js GET path

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,13 +1,17 @@
+// Add Postgres to the file
 const pg = require('pg');
 
+// Import Pool from pg
 const Pool = pg.Pool;
 
+// Creat new Pool object
 const pool = new Pool({
   database: 'react_gallery',
   host: 'localhost',
   port: 5432,
 });
 
+// Database Checks
 pool.on('connect', () => {
   console.log('Connected to the database');
 });
